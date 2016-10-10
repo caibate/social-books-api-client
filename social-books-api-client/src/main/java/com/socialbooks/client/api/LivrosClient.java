c269e166422c00a6feb7468c01776befae84359c
caibate <caibate@gmail.com>
2016-10-10 01:24:01 -0300

    Client java

--- social-books-api-client/src/main/java/com/socialbooks/client/api/LivrosClient.java
+++ social-books-api-client/src/main/java/com/socialbooks/client/api/LivrosClient.java
@@ -2,6 +2,7 @@ package com.socialbooks.client.api;
 
 import java.net.URI;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.http.RequestEntity;
@@ -11,15 +12,39 @@ import org.springframework.web.client.RestTemplate;
 
 import com.socialbooks.client.entity.Livro;
 
-@Component
+
 public class LivrosClient {
-	private static String url = "http://localhost:8080/livros";
+	private String URI_BASE = "http://localhost:8080/livros";
+	private StringBuffer authorization = new StringBuffer();
+	private RestTemplate restTemplate;
+	
+	public LivrosClient(String nome, String senha) {
+		restTemplate = new RestTemplate();
+		authorization.append("Basic ");
+	    authorization.append(Base64.getEncoder().encodeToString((nome + ":" + senha).getBytes()));
+		
+	}
 	
 	public List<Livro> listar() {
-		RestTemplate restTemplate = new RestTemplate();
-		
-		RequestEntity<Void> requestEntity = RequestEntity.get(URI.create(url)).header("Authorization", "Basic dGVzdGU6MQ==").build();
+		RequestEntity<Void> requestEntity = RequestEntity.get(URI.create(URI_BASE)).
+				header("Authorization", authorization.toString()).build();
 		ResponseEntity<Livro[]> response = restTemplate.exchange(requestEntity, Livro[].class);
 		return Arrays.asList(response.getBody());
+	}
+	
+	public String salvar(Livro livro){
+		RequestEntity<Livro> requestEntity = RequestEntity.
+				post(URI.create(URI_BASE)).header("Authorization", authorization.toString()).
+				body(livro);
+		ResponseEntity<Void> response = restTemplate.exchange(requestEntity, Void.class);
+		return response.getHeaders().getLocation().toString();
+	}
+	
+	public Livro buscar(String uri){
+		RequestEntity<Void> requestEntity = RequestEntity.
+				get(URI.create(uri)).header("Authorization", authorization.toString()).
+				build();
+		ResponseEntity<Livro> response = restTemplate.exchange(requestEntity, Livro.class);
+		return response.getBody();
 	}
 }
--- social-books-api-client/src/main/java/com/socialbooks/client/controller/ControllerSocialBooksAPI.java
+++ social-books-api-client/src/main/java/com/socialbooks/client/controller/ControllerSocialBooksAPI.java
@@ -1,6 +1,9 @@
 
 package com.socialbooks.client.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.springframework.stereotype.Controller;
 
 import com.socialbooks.client.api.LivrosClient;
@@ -9,17 +12,38 @@ import com.socialbooks.client.entity.Livro;
 public class ControllerSocialBooksAPI {
 	private LivrosClient livrosClient;
 	
-	public ControllerSocialBooksAPI() {
+	public ControllerSocialBooksAPI() throws ParseException {
 		System.out.println("Controler");
-		livrosClient = new LivrosClient();
+		livrosClient = new LivrosClient("teste", "1");
 		enviarRequisicao();
 	}
 	
-	public void enviarRequisicao(){
+	public void enviarRequisicao() throws ParseException{
 		System.out.println("*** ENVIA REQUISIÇÃO ***");
-		for(Livro livro: livrosClient.listar()){
-				System.out.println("Nome do livro" + livro.getNome());
+		
+		Livro livro = new Livro();
+		// Livro 1 
+		livro.setNome("History and Life");
+		livro.setEditora("Caibas Editora");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		livro.setPublicacao(sdf.parse("11/08/1987"));
+		livro.setResumo("Livro aborda vida, obra e história de Caibaté");
+		
+		String localizacao =livrosClient.salvar(livro);
+		livro = null;
+		System.out.println("- Localização do livro salvo: " + localizacao);
+		
+		//busca livros
+		livro = livrosClient.buscar(localizacao);
+		System.out.println("- Nome do livro persistido: " + livro.getNome() + System.lineSeparator());
+		
+		
+		//Lista Livros
+		for(Livro l: livrosClient.listar()){
+				System.out.println("- Nome do livro: " + l.getNome() + System.lineSeparator());
 		}
+		
+		
 	}
 	
 }
